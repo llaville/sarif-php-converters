7d94c4eaf9f486ffdf481fe637c7b83fa579ce8c
Laurent Laville <laurent.laville@gmail.com>
2024-12-04 07:22:43 +0000

    upgrade BOX Manifest support to v4.1.1

--- resources/box-manifest.sh
+++ resources/box-manifest.sh
@@ -2,10 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-curl -Ls https://github.com/llaville/box-manifest/releases/latest/download/box-manifest.phar -o $SCRIPT_DIR/box-manifest
+BOX_MANIFEST_VERSION="4.1.1"
+curl -Ls "https://github.com/llaville/box-manifest/releases/download/$BOX_MANIFEST_VERSION/box-manifest.phar" -o $SCRIPT_DIR/box-manifest
 chmod +x $SCRIPT_DIR/box-manifest
 
-$SCRIPT_DIR/box-manifest manifest:build --ansi -vv -c box.json --output-file=sbom.json
-$SCRIPT_DIR/box-manifest manifest:build --ansi -vv -c box.json --output-file=console.txt --format console
-$SCRIPT_DIR/box-manifest manifest:stub  --ansi -vv -c box.json --output-file=stub.php --resource console.txt --resource sbom.json
-#$SCRIPT_DIR/box compile --ansi -vv -c box.json.dist
+$SCRIPT_DIR/box-manifest make build stub configure -r console-table.txt -r plain.txt -r sbom.json -d $SCRIPT_DIR/.. --output-stub stub.php --output-conf box.json.dist -vvv --ansi
